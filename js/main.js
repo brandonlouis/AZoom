32d0c6f98c3a5b6f5afb9b8ace5fe6c7f36709ce
Brandon <brandonlouis1998@hotmail.com>
2022-10-24 20:49:24 +0800

    responsive navbar complete overhaul navbar elements removed confetti reoptimize CSS tidy up folder files

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
 const observer = new IntersectionObserver(entries => {
     // Loop over the entries
     entries.forEach(entry => {
-      // If the element is visible
-      if (entry.isIntersecting) {
-        // Add the animation class
-        entry.target.classList.add('animateScrollUp');
-        observer.unobserve(entry.target); // Stops observing to preserve memory
-      }
+        if (entry.isIntersecting) { // If the element is visible
+            entry.target.classList.add('animateScrollUp'); // Add the animation class
+            observer.unobserve(entry.target); // Stops observing to preserve memory
+        }
     });
-  });
+});
   
-  document.querySelectorAll('.animateOnScroll').forEach(element => {
+document.querySelectorAll('.animateOnScroll').forEach(element => {
     observer.observe(element);
-  });
+});
 
+function dropDownNav() {
+    const modalNavContainer = document.getElementById("modalNavContainer");
+    if (modalNavContainer.style.display == "") {
+        modalNavContainer.style.display = "flex";
+    } else {
+        modalNavContainer.style.display = "";
+    }
+}
 
 // Login popup; Closes form when user clicks outside of it.
 window.onclick = function closeModal(event) {
@@ -31,7 +37,6 @@ document.getElementById("loginBtn").addEventListener("click", (e) => {
     const loginForm = document.getElementById("loginContent");
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-
     if (username === "Bob" && password === "123") { // Username and password
         alert("Welcome "+ username + ", you have successfully logged in.");
         document.cookie = "username=" + username + "; path=/"; // Sets path to be default, allows deleting of this cookie from any pages
@@ -128,13 +133,10 @@ cardNumber.onkeyup = function() {
     
     if (cardNumber.value.replace(/\s/g, "").match(visaRegEx)) { // Toggles respective card that has been keyed in, based on regex
         visa.classList.remove("transparent");
-        
     } else if (cardNumber.value.replace(/\s/g, "").match(masterRegEx)) {
         mastercard.classList.remove("transparent");
-
     } else if (cardNumber.value.replace(/\s/g, "").match(amexRegEx)) {
         amex.classList.remove("transparent");
-        
     }
 }
 
@@ -159,128 +161,4 @@ document.getElementById("creditCardContent").addEventListener("submit", (e) => {
             timeleft -= 1;
         }, 1000);
     }
-    
 })
-
-
-
-
-
-
-
-// canvas = document.getElementById("canvas");
-// ctx = canvas.getContext("2d");
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
-// cx = ctx.canvas.width/2;
-// cy = ctx.canvas.height/2;
-
-// let confetti = [];
-// const confettiCount = 100;
-// const gravity = 0.5;
-// const terminalVelocity = 5;
-// const drag = 0.075;
-// const colors = [
-//   { front : "red", back: "darkred"},
-//   { front : "green", back: "darkgreen"},
-//   { front : "blue", back: "darkblue"},
-//   { front : "yellow", back: "darkyellow"},
-//   { front : "orange", back: "darkorange"},
-//   { front : "pink", back: "darkpink"},
-//   { front : "purple", back: "darkpurple"},
-//   { front : "turquoise", back: "darkturquoise"},
-// ];
-
-// //-----------Functions--------------
-// resizeCanvas = () => {
-// 	canvas.width = window.innerWidth;
-// 	canvas.height = window.innerHeight;
-// 	cx = ctx.canvas.width/2;
-// 	cy = ctx.canvas.height/2;
-// }
-
-// randomRange = (min, max) => Math.random() * (max - min) + min
-
-// initConfetti = () => {
-//   for (let i = 0; i < confettiCount; i++) {
-//     confetti.push({
-//       color      : colors[Math.floor(randomRange(0, colors.length))],
-//       dimensions : {
-//         x: randomRange(10, 20),
-//         y: randomRange(10, 30),
-//       },
-//       position   : {
-//         x: randomRange(0, canvas.width),
-//         y: canvas.height - 1,
-//       },
-//       rotation   : randomRange(0, 2 * Math.PI),
-//       scale      : {
-//         x: 1,
-//         y: 1,
-//       },
-//       velocity   : {
-//         x: randomRange(-25, 25),
-//         y: randomRange(0, -50),
-//       },
-//     });
-//   }
-// }
-
-// //---------Render-----------
-// render = () => {  
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
-//   confetti.forEach((confetto, index) => {
-//     let width = (confetto.dimensions.x * confetto.scale.x);
-//     let height = (confetto.dimensions.y * confetto.scale.y);
-    
-//     // Move canvas to position and rotate
-//     ctx.translate(confetto.position.x, confetto.position.y);
-//     ctx.rotate(confetto.rotation);
-    
-//     // Apply forces to velocity
-//     confetto.velocity.x -= confetto.velocity.x * drag;
-//     confetto.velocity.y = Math.min(confetto.velocity.y + gravity, terminalVelocity);
-//     confetto.velocity.x += Math.random() > 0.5 ? Math.random() : -Math.random();
-    
-//     // Set position
-//     confetto.position.x += confetto.velocity.x;
-//     confetto.position.y += confetto.velocity.y;
-    
-//     // Delete confetti when out of frame
-//     if (confetto.position.y >= canvas.height) confetti.splice(index, 1);
-
-//     // Loop confetto x position
-//     if (confetto.position.x > canvas.width) confetto.position.x = 0;
-//     if (confetto.position.x < 0) confetto.position.x = canvas.width;
-
-//     // Spin confetto by scaling y
-//     confetto.scale.y = Math.cos(confetto.position.y * 0.1);
-//     ctx.fillStyle = confetto.scale.y > 0 ? confetto.color.front : confetto.color.back;
-     
-//     // Draw confetto
-//     ctx.fillRect(-width / 2, -height / 2, width, height);
-    
-//     // Reset transform matrix
-//     ctx.setTransform(1, 0, 0, 1, 0, 0);
-//   });
-
-//   // Fire off another round of confetti
-//   if (confetti.length <= 10) initConfetti();
-
-//   window.requestAnimationFrame(render);
-// }
-
-// //---------Execution--------
-// initConfetti();
-// render();
-
-// //----------Resize----------
-// window.addEventListener("resize", function () {
-//   resizeCanvas();
-// });
-
-// //------------Click------------
-// window.addEventListener("click", function() {
-//   initConfetti();
-// });
